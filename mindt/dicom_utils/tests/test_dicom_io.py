37d219491acbc64808223922d69d53614bc7e199
salma <salmabougacha@hotmail.com>
2017-03-28 20:43:57 +0200

    test only error is raised for siemens dicoms

--- mindt/dicom_utils/tests/test_dicom_io.py
+++ mindt/dicom_utils/tests/test_dicom_io.py
@@ -1,5 +1,5 @@
 import os
-from nose.tools import assert_equal
+from nose.tools import assert_raises
 from mindt.dicom_utils import dicom_io
 from pkg_resources import Requirement, resource_filename
 test_dir = resource_filename(Requirement.parse("pydicom"), "dicom/testfiles")
@@ -9,8 +9,6 @@ testcharset_dir = resource_filename(Requirement.parse("pydicom"),
 dicom_filename = os.path.join(test_dir, "MR_small.dcm")
 
 
-def test_set_slice_thikness():
-    thickness = .11
-    dcm = dicom_io.set_slice_thickness(dicom_filename, thickness, copy=True)
-
-    assert_equal(thickness, dicom_io._get_slice_thickness(dcm))
+def test_get_slice_thikness():
+    # Check error is raised for inexistant fields
+    assert_raises(ValueError, dicom_io._get_slice_thickness, dicom_filename)
