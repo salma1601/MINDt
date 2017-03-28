e9a0154fae1eabc6bd8a480a0cd2b4a8a534cbda
salma <salmabougacha@hotmail.com>
2017-03-28 21:41:17 +0200

    better folder and module naming

--- mindt/bruker/tests/test_dicom.py
+++ mindt/bruker/tests/test_dicom.py
@@ -2,7 +2,7 @@ import os
 from nose.tools import assert_raises, assert_true, assert_equal
 import dicom
 
-from mindt.dicom_utils import dicom_io
+from mindt.bruker import dicoms
 
 from pkg_resources import Requirement, resource_filename
 test_dir = resource_filename(Requirement.parse("pydicom"), "dicom/testfiles")
@@ -13,12 +13,12 @@ dicom_filename = os.path.join(test_dir, "MR_small.dcm")
 
 
 def test_load_dcm():
-    dcm = dicom_io.load_dcm(dicom_filename)
+    dcm = dicoms.load_dcm(dicom_filename)
     assert_true(isinstance(dcm, dicom.dataset.FileDataset))
-    assert_equal(dcm, dicom_io.load_dcm(dcm))
-    assert_raises(TypeError, dicom_io.load_dcm, 1.)
+    assert_equal(dcm, dicoms.load_dcm(dcm))
+    assert_raises(TypeError, dicoms.load_dcm, 1.)
 
 
 def test_get_slice_thikness():
     # Check error is raised for inexistant fields
-    assert_raises(ValueError, dicom_io._get_slice_thickness, dicom_filename)
+    assert_raises(ValueError, dicoms._get_slice_thickness, dicom_filename)
